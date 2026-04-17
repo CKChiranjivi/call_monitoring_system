bcc973efdca745f286fef14f43c95d3d85088151
Chanchal Kumar Chiranjivi <165770818+CKChiranjivi@users.noreply.github.com>
2026-04-17 13:57:24 +0530

    commit

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Make sure to properly escape special characters in the password if needed
-DATABASE_URL = "mysql+pymysql://root:Chanchal@123@localhost/call_monitor"
+DATABASE_URL = "mysql+pymysql://root:Chanchal@123@localhost/epbx_logs"
 
 engine = create_engine(DATABASE_URL)
 
